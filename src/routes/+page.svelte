a0b9cf39117e9388a74f57cf598d08be387607ed
askadityapandey <askadityapandey@gmail.com>
2024-06-22 12:49:26 +0530

    intial commits

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    const cloudName = ''; // Replace with your Cloudinary cloud name
+    const imageId = 'w-1';
+    const imageUrl = `https://res.cloudinary.com/${cloudName}/image/upload/${imageId}`;
+
+    // Provide meaningful alt text for the image
+    const altText = 'Description of the image'; // Replace with an actual description of the image
+</script>
+
+<style>
+    .image {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
+
+<img src={imageUrl} alt={altText} class="image">
